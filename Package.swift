c0d6f0b675fb07c505835a4d52fbe34176758419
Joshua Goossen <jigoossen@gmail.com>
2021-03-09 14:38:46 -0300

    Added Publish as dependency for theme

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     dependencies: [
        // .package(path: "../SimpleSassPublishPlugin"),
+        .package(path: "../Publish"),
 
     ],
     targets: [
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SearaPublishTheme",
-            dependencies: []),
+            dependencies: ["Publish"]),
         .testTarget(
             name: "SearaPublishThemeTests",
             dependencies: ["SearaPublishTheme"]),
